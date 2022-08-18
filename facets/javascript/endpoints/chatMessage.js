4f6964be6336ba773fd7fc368de1baa43c1c50a8
meveo.admin <no-mail@meveo.com>
2022-08-18 07:02:50 +0000

    Create JS script for endpointchatMessage

--- facets/javascript/endpoints/chatMessage.js
+++ facets/javascript/endpoints/chatMessage.js
@@ -1,34 +1,46 @@
-const chatMessage = async (parameters) =>  {
-	const baseUrl = window.location.origin;
-	const url = new URL(`${window.location.pathname.split('/')[1]}/rest/chatMessage/`, baseUrl);
-	return fetch(url.toString(), {
-		method: 'POST', 
-		headers : new Headers({
- 			'Content-Type': 'application/json'
-		}),
-		body: JSON.stringify({
-			
-		})
-	});
+import EndpointInterface from "#{API_BASE_URL}/api/rest/endpoint/EndpointInterface.js";
+
+// the request schema, this should be updated
+// whenever changes to the endpoint parameters are made
+// this is important because this is used to validate and parse the request parameters
+const requestSchema = {
+  "title" : "chatMessageRequest",
+  "id" : "chatMessageRequest",
+  "default" : "Schema definition for chatMessage",
+  "$schema" : "http://json-schema.org/draft-07/schema",
+  "type" : "object"
 }
 
-const chatMessageForm = (container) => {
-	const html = `<form id='chatMessage-form'>
-		<button type='button'>Test</button>
-	</form>`;
-
-	container.insertAdjacentHTML('beforeend', html)
-
-
-	container.querySelector('#chatMessage-form button').onclick = () => {
-		const params = {
-
-		};
-
-		chatMessage(params).then(r => r.text().then(
-				t => alert(t)
-			));
-	};
+// the response schema, this should be updated
+// whenever changes to the endpoint parameters are made
+// this is important because this could be used to parse the result
+const responseSchema = {
+  "title" : "chatMessageResponse",
+  "id" : "chatMessageResponse",
+  "default" : "Schema definition for chatMessage",
+  "$schema" : "http://json-schema.org/draft-07/schema",
+  "type" : "object"
 }
 
-export { chatMessage, chatMessageForm };
+// should contain offline mock data, make sure it adheres to the response schema
+const mockResult = {};
+
+class chatMessage extends EndpointInterface {
+	constructor() {
+		// name and http method, these are inserted when code is generated
+		super("chatMessage", "POST");
+		this.requestSchema = requestSchema;
+		this.responseSchema = responseSchema;
+		this.mockResult = mockResult;
+	}
+
+	getRequestSchema() {
+		return this.requestSchema;
+	}
+
+	getResponseSchema() {
+		return this.responseSchema;
+	}
+}
+
+export default new chatMessage();
