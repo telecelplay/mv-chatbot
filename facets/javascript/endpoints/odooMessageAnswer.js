1717ba3a8a1b6d4324eef8799bcc3232343094b3
meveo.admin <no-mail@meveo.com>
2022-08-19 23:41:47 +0000

    Add JS script for Endpoint: odooMessageAnswer

--- facets/javascript/endpoints/odooMessageAnswer.js
+++ facets/javascript/endpoints/odooMessageAnswer.js
@@ -1,53 +1,33 @@
-import EndpointInterface from "#{API_BASE_URL}/api/rest/endpoint/EndpointInterface.js";
-
-// the request schema, this should be updated
-// whenever changes to the endpoint parameters are made
-// this is important because this is used to validate and parse the request parameters
-const requestSchema = {
-  "title" : "odooMessageAnswerRequest",
-  "id" : "odooMessageAnswerRequest",
-  "default" : "Schema definition for odooMessageAnswer",
-  "$schema" : "http://json-schema.org/draft-07/schema",
-  "type" : "object"
+const odooMessageAnswer = async (parameters) =>  {
+	const baseUrl = window.location.origin;
+	const url = new URL(`${window.location.pathname.split('/')[1]}/rest/odooMessageAnswer/${parameters.messageId}`, baseUrl);
+	return fetch(url.toString(), {
+		method: 'GET'
+	});
 }
 
-// the response schema, this should be updated
-// whenever changes to the endpoint parameters are made
-// this is important because this could be used to parse the result
-const responseSchema = {
-  "title" : "odooMessageAnswerResponse",
-  "id" : "odooMessageAnswerResponse",
-  "default" : "Schema definition for odooMessageAnswer",
-  "$schema" : "http://json-schema.org/draft-07/schema",
-  "type" : "object",
-  "properties" : {
-    "result" : {
-      "title" : "result",
-      "type" : "string",
-      "minLength" : 1
-    }
-  }
+const odooMessageAnswerForm = (container) => {
+	const html = `<form id='odooMessageAnswer-form'>
+		<div id='odooMessageAnswer-messageId-form-field'>
+			<label for='messageId'>messageId</label>
+			<input type='text' id='odooMessageAnswer-messageId-param' name='messageId'/>
+		</div>
+		<button type='button'>Test</button>
+	</form>`;
+
+	container.insertAdjacentHTML('beforeend', html)
+
+	const messageId = container.querySelector('#odooMessageAnswer-messageId-param');
+
+	container.querySelector('#odooMessageAnswer-form button').onclick = () => {
+		const params = {
+			messageId : messageId.value !== "" ? messageId.value : undefined
+		};
+
+		odooMessageAnswer(params).then(r => r.text().then(
+				t => alert(t)
+			));
+	};
 }
 
-// should contain offline mock data, make sure it adheres to the response schema
-const mockResult = {};
-
-class odooMessageAnswer extends EndpointInterface {
-	constructor() {
-		// name and http method, these are inserted when code is generated
-		super("odooMessageAnswer", "GET");
-		this.requestSchema = requestSchema;
-		this.responseSchema = responseSchema;
-		this.mockResult = mockResult;
-	}
-
-	getRequestSchema() {
-		return this.requestSchema;
-	}
-
-	getResponseSchema() {
-		return this.responseSchema;
-	}
-}
-
-export default new odooMessageAnswer();
+export { odooMessageAnswer, odooMessageAnswerForm };
